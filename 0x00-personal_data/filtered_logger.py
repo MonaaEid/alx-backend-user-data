994b6a53df9406bbabd261b9afb1df972a205d94
MonaaEid <monaa_eid@hotmail.com>
2024-05-17 02:26:44 +0300

    Add filter_datum function in filtered_logger script

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,12 +4,26 @@ the log message obfuscated"""
 
 import re
 from typing import List
+import logging
 
 
-def filter_datum(fields: List[str], redaction: str,
-                    message: str, separator: str) -> str:
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
+
+
+def filter_datum(fields: List[str], redaction: str,message: str, separator: str) -> str:
         """returns the log message obfuscated"""
         for field in fields:
-            message = re.sub(r'{}=.*?{}'.format(field, separator), '{}={}{}'
-                          .format(field, redaction, separator), message)
+            message = re.sub(r'{}=.*?{}'.format(field, separator), '{}={}{}'.format(field, redaction, separator), message)
         return message
