523414fbd7005ab57cae26bdda90b89c09780d77
MonaaEid <monaa_eid@hotmail.com>
2024-05-17 02:19:14 +0300

    Add filter_datum function in filtered_logger script

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,6 +10,6 @@ def filter_datum(fields: List[str], redaction: str,
                     message: str, separator: str) -> str:
         """returns the log message obfuscated"""
         for field in fields:
-            message = re.sub(rf'{field}=.+?{separator}',
-                            f'{field}={redaction}{separator}', message)
+            message = re.sub(r'{}=.*?{}'.format(field, separator), '{}={}{}'
+                          .format(field, redaction, separator), message)
         return message
