1e58294ad569ad1bcf3cc663dfb9416b4114cfdc
MonaaEid <monaa_eid@hotmail.com>
2024-05-17 03:39:42 +0300

    Add filter_datum function in filtered_logger script

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -34,6 +34,7 @@ class RedactingFormatter(logging.Formatter):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
+
     def format(self, record: logging.LogRecord) -> str:
         """Format the record
         """
