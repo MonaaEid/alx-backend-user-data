fe7f88c96b6c09bcde5336e21868fa3a56fac5f6
MonaaEid <monaa_eid@hotmail.com>
2024-05-21 01:04:56 +0300

    Add api dir

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -6,17 +6,14 @@ from typing import List, TypeVar
 
 class Auth:
     """class Auth"""
-
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """"""
         return False
 
-
     def authorization_header(self, request=None) -> str:
-        """"""
+        """authorization_header"""
         return None
 
-
     def current_user(self, request=None) -> TypeVar('User'):
-        """"""
+        """current_user"""
         return None
