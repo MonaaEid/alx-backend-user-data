5c0bb6f254435fdc62f62794ed48cee236ad00ee
MonaaEid <monaa_eid@hotmail.com>
2024-05-22 01:30:36 +0300

    Add api dir

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -28,22 +28,19 @@ if auth_type == 'basic_auth':
 @app.before_request
 def before_request_handler():
     """before_request_handler"""
-    if auth is None:
-        return
-    if request.path in [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-            '/api/v1/forbidden/']:
-        return
-    if not auth.require_auth(request.path,
-                             ['/api/v1/status/',
-                              '/api/v1/unauthorized/',
-                              '/api/v1/forbidden/']):
-        return
-    if auth.authorization_header(request) is None:
-        abort(401)
-    if auth.current_user(request) is None:
-        abort(403)
+    if auth:
+        excluded_paths = [
+            '/api/v1/status/',
+            '/api/v1/unauthorized/',
+            '/api/v1/forbidden/',
+        ]
+        if auth.require_auth(request.path, excluded_paths):
+            auth_header = auth.authorization_header(request)
+            user = auth.current_user(request)
+            if auth_header is None:
+                abort(401)
+            if user is None:
+                abort(403)
 
 
 @app.errorhandler(404)
