446b8561da0cec7e024f0267c69345d769aedafc
MonaaEid <monaa_eid@hotmail.com>
2024-05-22 01:48:13 +0300

    Add api dir

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,7 +15,6 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
-AUTH_TYPE = os.getenv('AUTH_TYPE')
 
 auth_type = getenv('AUTH_TYPE', 'auth')
 if auth_type == 'auth':
