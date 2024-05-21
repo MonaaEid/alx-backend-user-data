618b467d4d65167fbab644d457bb407e92c93dae
MonaaEid <monaa_eid@hotmail.com>
2024-05-21 20:26:00 +0300

    Add api dir

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -21,6 +21,7 @@ if AUTH_TYPE == 'auth':
 
 @app.before_request
 def before_request_handler():
+    """before_request_handler"""
     if auth is None:
         return
     if request.path in ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']:
