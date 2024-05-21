1a6a6bb75d54a6dacaefd46f4be473803e877a65
MonaaEid <monaa_eid@hotmail.com>
2024-05-22 01:13:59 +0300

    Add api dir

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,6 +7,8 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
@@ -15,13 +17,12 @@ CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 AUTH_TYPE = os.getenv('AUTH_TYPE')
 
-if AUTH_TYPE == 'basic_auth':
-    from api.v1.auth.basic_auth import BasicAuth
-    auth = BasicAuth()
-else:
-    from api.v1.auth.auth import Auth
+auth = None
+auth_type = getenv('AUTH_TYPE', 'auth')
+if auth_type == 'auth':
     auth = Auth()
-
+if auth_type == 'basic_auth':
+    auth = BasicAuth()
 
 @app.before_request
 def before_request_handler():
