796794e3ae7ba2168670dff8a3ab005d43630d02
MonaaEid <monaa_eid@hotmail.com>
2024-05-26 00:17:31 +0300

    Add api dir

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -8,7 +8,8 @@ import base64
 from api.v1.auth.auth import Auth
 from typing import TypeVar
 from models.user import User
-
+from api.v1.auth.auth import Auth
+import uuid
 
 class SessionAuth(Auth):
     """SessionAuth class"""
@@ -20,7 +21,7 @@ class SessionAuth(Auth):
             return None
         if not isinstance(user_id, str):
             return None
-        session_id = super().generate_id()
+        session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
 
