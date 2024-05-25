9a1281bba4f77da1a46bbfb1c88a31a1e88fa6b6
MonaaEid <monaa_eid@hotmail.com>
2024-05-26 00:45:05 +0300

    Add api dir

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -60,7 +60,8 @@ def before_request_handler():
     ]
     if not auth.require_auth(request.path, excluded_paths):
         return
-    if auth.authorization_header(request) is None:
+    if auth.authorization_header(request) is None \
+        and auth.session_cookie(request) is None:
         abort(401)
     request.current_user = auth.current_user(request)
     if request.current_user is None:
