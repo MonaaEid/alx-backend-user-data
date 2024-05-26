0970ff4637bebf5676ba3a78bb62a77a65cb7f3d
MonaaEid <monaa_eid@hotmail.com>
2024-05-26 03:21:58 +0300

    Add api dir

---
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""Session authentication views"""
+from api.v1.views import app_views
+from flask import jsonify, request, abort
+from models.user import User
+import os
+from api.v1.auth.session_auth import SessionAuth
+
+
+@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
+def login():
+    """ POST /api/v1/auth_session/login
+    Return:
+      - User object JSON represented
+      - 400 if the User ID doesn't exist
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if email is None or email == "":
+        return jsonify({"error": "email missing"}), 400
+    if password is None or password == "":
+        return jsonify({"error": "password missing"}), 400
+    user = User.search({'email': email})
+    if user is None or not user:
+        return jsonify({"error": "no user found for this email"}), 404
+    if not user[0].is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+    from api.v1.app import auth
+    session_id = auth.create_session(user[0].id)
+    response = jsonify(user[0].to_json())
+    response.set_cookie(os.getenv('SESSION_NAME'), session_id)
+    return response
+
+
+@app_views.route('/auth_session/logout', methods=['DELETE'],
+                 strict_slashes=False)
+def logout():
+    """ DELETE /api/v1/auth_session/logout
+    Return:
+      - empty JSON
+    """
+    from api.v1.app import auth
+    if auth.destroy_session(request) is False:
+        abort(404)
+    return jsonify({}), 200
+
+
+@app_views.route('/auth_session/profile',
+                 methods=['GET'], strict_slashes=False)
+def profile():
+    """ GET /api/v1/auth_session/profile
+    Return:
+      - User object JSON represented
+      - 404 if the User ID doesn't exist
+    """
+    from api.v1.app import auth
+    user = auth.current_user(request)
+    if user is None:
+        abort(404)
+    return jsonify(user.to_json())
