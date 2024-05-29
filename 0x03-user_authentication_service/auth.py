370eaae0acdaff57990703693b436e259afe5223
MonaaEid <monaa_eid@hotmail.com>
2024-05-30 00:16:26 +0300

    Add api dir

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -13,3 +13,24 @@ def _hash_password(password: str) -> str:
     """Hash a password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class
+    """
+
+    def __init__(self):
+        """Initialize a new Auth instance
+        """
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Register a user
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f"User {email} already exists")
+        except NoResultFound:
+            user = self._db.add_user(email, _hash_password(password))
+            return user
