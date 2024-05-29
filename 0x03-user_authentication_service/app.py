fe7958c3e3209223622eb2e01c28b49af55848e8
MonaaEid <monaa_eid@hotmail.com>
2024-05-30 02:29:01 +0300

    Add api dir

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -95,3 +95,19 @@ def get_reset_password_token() -> str:
         return jsonify({"email": email, "reset_token": reset_token}), 200
     except ValueError:
         abort(403)
+
+@app.route('/reset_password', methods=['PUT'], strict_slashes=False)
+def update_password() -> str:
+    """PUT /reset_password
+    Update password
+    Return:
+      - message
+    """
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    new_password = request.form.get('new_password')
+    try:
+        AUTH.update_password(reset_token, new_password)
+        return jsonify({"email": email, "message": "Password updated"}), 200
+    except ValueError:
+        abort(403)
