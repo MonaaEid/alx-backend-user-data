068a3608629fdac5144fd866b05b4726da5b391f
MonaaEid <monaa_eid@hotmail.com>
2024-05-30 03:58:20 +0300

    Add api dir

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -96,6 +96,7 @@ def get_reset_password_token() -> str:
     except ValueError:
         abort(403)
 
+
 @app.route('/reset_password', methods=['PUT'], strict_slashes=False)
 def update_password() -> str:
     """PUT /reset_password
